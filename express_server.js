3bf5d77945b3374b20b11d06ab5151738ffce7d3
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-09 21:53:34 +0000

    added comment section for highlighting scope accessibility

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/urls.json", (req, res) => {
-  // we are seeing the urlDB object as a string in the browser
+  // we are seeing the urlDB object as a string in the browser. So stringy is happening here by express automatically when calling .json method
   res.json(urlDatabase)
 })
 
@@ -20,6 +20,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
 
+// the below code will not work as variable a is accessible only within /set page and not under /fetch page
+/*
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+*/
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
