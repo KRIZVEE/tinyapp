79b431b1e3d667c684791bfbf3cae02ecd2b9cbd
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-12 12:34:30 +0000

    added bcrypt

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
+const bcrypt = require('bcrypt')
 const app = express();
 const PORT = 8080; // default port 8080
 
